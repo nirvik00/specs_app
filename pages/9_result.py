84b987d2262eb842ef3cbaf7b6e0bc001160c9ed
Nirvik Saha <nsdev404@gmail.com>
2025-03-17 23:11:25 -0400

    ready!

--- pages/9_result.py
+++ pages/9_result.py
@@ -176,3 +176,55 @@ st.plotly_chart(fig, theme="streamlit")
 #########################################################################
 
 st.dataframe(res_df)
+
+#########################################################################
+#
+#                       sunburst plot
+#
+#########################################################################
+
+def plot_sunburst():
+    sec_nums = st.session_state['result_sec_nums'] 
+    sec_names = st.session_state['result_sec_names']
+    unique_div_nums = []
+    for e in sec_nums:
+        f = e.split(" ")[0]
+        if f not in unique_div_nums:
+            unique_div_nums.append(f)
+
+    #
+    parents = [" "]
+    child=["root"]
+    count_li=[]
+    values=[1]
+    for e in unique_div_nums:
+        parents.append('root')
+        child.append(e)
+        values.append(1)
+        count=0
+        for sec in sec_nums:
+            if sec.split(" ")[0] == e:
+                count+=1
+        count_li.append(count)
+
+    #
+    for e in sec_nums:
+        f = e.split(" ")[0]
+        if f in unique_div_nums:
+            parents.append(f)
+            child.append(e)
+            x = unique_div_nums.index(f)
+            values.append(count_li[x])
+    #
+    return parents, child, values
+
+(parents, child, values) = plot_sunburst()
+
+# df = pd.DataFrame({'child':child, 'parents':parents, 'values': values})
+# fig = px.sunburst(df, names = 'child', parents = 'parents', values='values')
+
+data = dict( child=child, parents=parents)
+fig = px.sunburst(data, names = 'child', parents = 'parents')
+
+# Plot!
+st.plotly_chart(fig, theme="streamlit")
